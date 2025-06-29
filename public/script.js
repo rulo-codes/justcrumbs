5e19adad26d1ae1ffc340967210dc14b16bb2242
rulo-codes <150222307+rulo-codes@users.noreply.github.com>
2025-06-29 17:56:52 +0800

    20250625-1756: Added responsiveness and burger button

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const burger = document.querySelector("#burger");
+const nav = document.querySelector("#navLinks");
+const navlinks = document.querySelectorAll("#nav-link");
+
+let menuOpen = false;
+
+// Toggle menu
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-menu");
+  menuOpen = !menuOpen;
+  console.log(menuOpen ? "menu open" : "menu closed");
+});
+
+
+navlinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth <= 800 && menuOpen) {
+      nav.classList.toggle("nav-menu");
+      menuOpen = false;
+      console.log("link clicked, menu closed");
+    }
+  });
+});
